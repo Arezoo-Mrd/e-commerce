354a67b7aa0164567546fb847ddfa6eaedd32ee6
Arezoo Mordai <moradi.arezoo1992@gmail.com>
2023-11-23 13:21:11 +0330

    make create product controller

--- backend/src/controller/products.controller.ts
+++ backend/src/controller/products.controller.ts
@@ -22,3 +22,40 @@ export const getProducts = async (req: Request, res: Response): Promise<Response
         return res.status(Code.INTERNAL_SERVER_ERROR).send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'bad request'))
     }
 }
+
+export const getProduct = async (req: Request, res: Response): Promise<Response<IProduct[]>> => {
+    console.log(`${req.originalUrl} Resquest from ${req.rawHeaders[0]}`)
+    try {
+        const pool = await connection()
+        const result: ResultSet = await pool.query(QUERY.SELECT_PRODUCTS, [req.params.productId])
+        if ((result[0] as Array<ResultSet>).length > 0) {
+            return res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, `everything is ok`, result))
+        } else {
+            return res.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'product not found'))
+        }
+    } catch (error: unknown) {
+        console.error(error)
+        return res.status(Code.INTERNAL_SERVER_ERROR).send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'bad request'))
+    }
+}
+
+
+
+
+// TODO start at here
+
+export const createProduct = async (req: Request, res: Response): Promise<Response<IProduct>> => {
+    console.log(`${req.originalUrl} Resquest from ${req.rawHeaders[0]}`)
+    try {
+        const pool = await connection()
+        const result: ResultSet = await pool.query(QUERY.SELECT_PRODUCTS, [req.params.productId])
+        if ((result[0] as Array<ResultSet>).length > 0) {
+            return res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, `everything is ok`, result))
+        } else {
+            return res.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'product not found'))
+        }
+    } catch (error: unknown) {
+        console.error(error)
+        return res.status(Code.INTERNAL_SERVER_ERROR).send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'bad request'))
+    }
+}
